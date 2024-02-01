53b1ea9011c4713f649c217d4609afdc708c6471
fluffyhake <haakon.reppen@gmail.com>
2024-02-01 23:25:30 +0100

    +gallery component

--- frontend/skinkis/src/app/gallery/gallery.component.ts
+++ frontend/skinkis/src/app/gallery/gallery.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar'; 
+
+interface AllImagesResponse{
+  images: string[]
+}
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatProgressBarModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit{
+  private _isLoading = true
+  private _imageArr = new Array
 
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.http.get<AllImagesResponse>("http://localhost:8080/upload/🐦‍⬛🐦‍⬛🐦‍⬛").subscribe(response => {
+      for(let entry of response["images"]){
+        this._imageArr.push("http://localhost:8080/" + entry)
+
+      }
+    })
+    
+    this._isLoading = false
+  }
+  get isLoading(): boolean {
+    return this._isLoading
+  }
+  get imageArr(): ImageBitmap[]{
+    return this._imageArr
+  }
 }
+
